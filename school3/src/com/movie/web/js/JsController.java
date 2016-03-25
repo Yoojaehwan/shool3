a7df82af30be3ecf5e2401974150b488f632451c
류재환 <@172.168.0.52>
2016-03-25 10:05:01 +0900

    .

---
+++ school3/src/com/movie/web/js/JsController.java
@@ -0,0 +1,28 @@
+package com.movie.web.js;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.moive.web.global.Command;
+import com.moive.web.global.CommandFactory;
+import com.moive.web.global.DispatcherServlet;
+import com.moive.web.global.Seperator;
+import com.movie.web.memer.MemberBean;
+
+@WebServlet({"/js/hello.do","/js/var.do","/js/operator.do","/js/function.do",
+	"/js/object.do","/js/bom.do","/js/dom.do","/js/form.do","/js/closure.do","/js/pattern.do"})
+public class JsController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		String[] str = Seperator.divide(request, response);
+		DispatcherServlet.Go(request, response, CommandFactory.createCommand(str[1], str[0]));
+		
+	}
+}
